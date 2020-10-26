dfefee3cd242226ed2dec9a69b3a07a32bc3c908
y-tsai <yunchen224@gmail.com>
2020-10-26 16:36:23 +0800

    update table info

--- js/all.js
+++ js/all.js
@@ -3,7 +3,6 @@ const newPage = window.open("", "", "width=200,height=280");
 newPage.document.write(content);
 newPage.print();
 
-
 // var barcode_no = document.getElementById('type-barcode')
 
 // for(var i = 0; i <= barcode_no; i++){
