5abefc8c908b90230cbfb70676195f5bb920dd25
y-tsai <yunchen224@gmail.com>
2020-10-30 11:43:03 +0800

    css update
    1.取消下方no-line
    2.調整table height
    3.table2 hide

--- js/all.js
+++ js/all.js
@@ -1,10 +1,21 @@
-const content = document.querySelector(".print").innerHTML;
-const newPage = window.open("", "", "width=200,height=280");
-newPage.document.write(content);
-newPage.print();
+// const content = document.querySelector(".print").innerHTML;
+// const newPage = window.open("", "", "width=200,height=280");
+// newPage.document.write(content);
+// newPage.print();
 
 // var barcode_no = document.getElementById('type-barcode')
 
 // for(var i = 0; i <= barcode_no; i++){
 
 // }
+$(document).ready(function(e){
+    $("#inside-table2").hide();
+});
+
+fetch('https://backend.jin-ting.com.tw/api/items/product?fields=*.*&filter[name][eq]=JC-28NH').then(function(res){
+    return res.json();
+})
+.then(function(nht_json){
+    document.getElementById('brand').innerText= JSON.parse('company_name');
+
+})
