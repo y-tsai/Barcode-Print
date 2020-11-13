0b1c75191bd792d9a2884d8c76533b3a39c3cdcb
y-tsai <yunchen224@gmail.com>
2020-11-13 17:47:03 +0800

    UPDATE JS

---
+++ js/table.js
@@ -0,0 +1,31 @@
+var [
+    // table
+    brand,
+    product_name,
+    air_type,
+    power_phase_volt,
+    capacity_cool,
+    air_volume,
+    air_kg,
+    year,
+    company_name,
+    company_address,
+    sticker_no,
+    case_no,
+    type_barcode
+] = [
+    // table
+    brand,
+    product_name,
+    air_type,
+    power_phase_volt,
+    capacity_cool,
+    air_volume,
+    air_kg,
+    year,
+    company_name,
+    company_address,
+    sticker_no,
+    case_no,
+    type_barcode
+]
