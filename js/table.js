cb0754cb67a0fc36e5690efea40ec58141968442
y-tsai <yunchen224@gmail.com>
2020-11-19 14:05:22 +0800

    create modal

--- js/table.js
+++ js/table.js
@@ -1,5 +1,5 @@
 function ajaxRequest(params) {
-  var url = "https://backend.jin-ting.com.tw/api/items/barcode_print";
+  var url = "https://backend.jin-ting.com.tw/api/items/product?fields=*.*&filter[product_spec][neq]=";
   $.get(url).then(function (res) {
     params.success(res.data);
   });
