84e661dca8acaf53167cd8f34efce4a3522db65e
y-tsai <yunchen224@gmail.com>
2020-11-20 14:20:33 +0800

    table list update

--- js/table.js
+++ js/table.js
@@ -1,15 +1,28 @@
-
 function ajaxRequest(params) {
   var url = "https://backend.jin-ting.com.tw/api/items/product?fields=*.*&filter[product_spec][neq]=";
-  $.get(url).then(function (res) {
-    console.log(res.data);
-    params.success(res.data);
+
+  $.get(url).then(function (res) {   
+    var listable = res.data;
+    list = listable.map((product) => {
+      return {
+        brand: product.brand.name,
+        product_name: product.product_spec.product_name,
+        air_type: product.name,
+        power_phase: product.product_spec.power_phase,
+        power_volt: product.product_spec.power_volt,
+        capacity_cool: product.product_spec.capacity_cool,
+        capacity_heat: product.product_spec.capacity_heat,
+        air_width: product.product_spec.air_width,
+        air_height: product.product_spec.air_height,
+        air_depth: product.product_spec.air_depth,
+        air_kg: product.product_spec.air_kg,
+        company_name: product.brand.company_name,
+        company_tel: product.brand.company_tel,
+        company_address: product.brand.company_address,
+      };
+    });
+    params.success(list);
+    
   });
 }
-// $.ajax({
-//   url: 'https://backend.jin-ting.com.tw/api/items/product?fields=*.*&filter[product_spec][neq]=',// url位置
-//   type: 'get',                   // post/get
-//   data: { querytag: data },       // 輸入的資料
-//   // error: function (xhr) { },      // 錯誤後執行的函數
-//   // success: function (response) { }// 成功後要執行的函數
-// });
+
