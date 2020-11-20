bb45c2f9f9156bd99d6a2cb425ed088a9bad70d7
y-tsai <yunchen224@gmail.com>
2020-11-20 09:15:59 +0800

    update table.js

--- js/table.js
+++ js/table.js
@@ -1,6 +1,15 @@
+
 function ajaxRequest(params) {
   var url = "https://backend.jin-ting.com.tw/api/items/product?fields=*.*&filter[product_spec][neq]=";
   $.get(url).then(function (res) {
+    console.log(res.data);
     params.success(res.data);
   });
 }
+// $.ajax({
+//   url: 'https://backend.jin-ting.com.tw/api/items/product?fields=*.*&filter[product_spec][neq]=',// url位置
+//   type: 'get',                   // post/get
+//   data: { querytag: data },       // 輸入的資料
+//   // error: function (xhr) { },      // 錯誤後執行的函數
+//   // success: function (response) { }// 成功後要執行的函數
+// });
