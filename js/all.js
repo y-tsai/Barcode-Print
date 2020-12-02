6917e93fbcb965982e93b92294fb40ac1330dc41
y-tsai <yunchen224@gmail.com>
2020-12-02 13:05:39 +0800

    tablejs&printpagejs update

--- js/all.js
+++ js/all.js
@@ -3,14 +3,41 @@ $(document).ready(function(e){
     $("#inside-table2").hide();
 });
 
-JsBarcode("#type_barcode", "209942", {
-    format: "code128",
-    height: 70,
-    width: 2,
-    displayValue: true
-  });
+ function getTableData(){
 
- 
+  var getUrlString = location.href;
+  var url = new URL(getUrlString);
+  var barcodeNo = url.searchParams.get('barcode_no');
+  var airWidth = url.searchParams.get('air_width');
+  var airHeight = url.searchParams.get('air_height');
+  var airDepth = url.searchParams.get('air_depth');
+  var airVolume = airWidth + " * " + airHeight + " * " + airDepth;
+  
+
+  document.getElementById('brand').innerText = url.searchParams.get('brand');
+  document.getElementById('product_name').innerText = url.searchParams.get('product_name');
+  document.getElementById('air_type').innerText = url.searchParams.get('air_type');
+  document.getElementById('power_phase').innerText = url.searchParams.get('power_phase');
+  document.getElementById('power_volt').innerText = url.searchParams.get('power_volt');
+  document.getElementById('capacity_cool').innerText = url.searchParams.get('capacity_cool');
+  document.getElementById('air_volume').innerHTML = airVolume;
+  document.getElementById('air_kg').innerText = url.searchParams.get('air_kg');
+  document.getElementById('year').innerText = url.searchParams.get('year');
+  document.getElementById('company_name').innerText = url.searchParams.get('company_name');
+  document.getElementById('company_address').innerText = url.searchParams.get('company_address');
+  document.getElementById('case_no').innerText = url.searchParams.get('case_no');
+
+  
+  JsBarcode("#barcode_no", barcodeNo , {
+     format: "code128",
+     height: 70,
+     width: 2,
+     displayValue: true
+   });
+
+ }getTableData();   
+
+
 
 // fetch('https://backend.jin-ting.com.tw/api/items/product?fields=*.*')
 // .then(function(response){
--- js/table.js
+++ js/table.js
@@ -42,6 +42,7 @@ function ajaxRequest(params) {
 
 $barcodePrintTable.on("click-row.bs.table", function (e, row) {
   $barcodeModal.modal("show");
+
   document.getElementById("brand").value = row.brand;
   document.getElementById("air_type").value = row.air_type;
   document.getElementById("power_volt").value = row.power_volt;
@@ -49,17 +50,11 @@ $barcodePrintTable.on("click-row.bs.table", function (e, row) {
   document.getElementById("case_no").value = row.case_no;
 
   $("#btn-modal-confirm").on("click", function (e) {
-    var print_page = `href="Print_Page.html?brand=${row.brand}&product_name=${row.product_name}&air_type=${row.air_type}&power_phase=${row.power_phase}&power_volt=${row.power_volt}&capacity_cool=${row.capacity_cool}&capacity_heat=${row.capacity_heat}&air_width=${row.air_width}&air_height=${row.air_height}&air_depth=${row.air_depth}&air_kg=${row.air_kg}&company_name=${row.company_name}&company_tel=${row.company_tel}&company_address=${row.company_address}&sticker_no=${row.sticker_no}&case_no=${row.case_no}&year=${modal.year}&barcode_no=${modal.barcode_no}"`;
-    window.open('print_page', "Barcode_PrintPage");  
+    var barcode_no = $("#barcode_no").val();
+    var year = $("#year").val();
+    var case_no = $("#case_no").val();
+    var print_page = `Print_Page.html?brand=${row.brand}&product_name=${row.product_name}&air_type=${row.air_type}&power_phase=${row.power_phase}&power_volt=${row.power_volt}&capacity_cool=${row.capacity_cool}&capacity_heat=${row.capacity_heat}&air_width=${row.air_width}&air_height=${row.air_height}&air_depth=${row.air_depth}&air_kg=${row.air_kg}&company_name=${row.company_name}&company_tel=${row.company_tel}&company_address=${row.company_address}&sticker_no=${row.sticker_no}&case_no=${case_no}&year=${year}&barcode_no=${barcode_no}`;
+
+    window.open(print_page, "_blank");
   });
-
 });
-
-
-
-
-
-
-
-
-
