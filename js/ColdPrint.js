a886fc23982a8c0d843a92335eb4f78e39605308
y-tsai <yunchen224@gmail.com>
2020-12-15 17:47:56 +0800

    Export PDF js create

--- js/ColdPrint.js
+++ js/ColdPrint.js
@@ -17,31 +17,17 @@ function getTableData() {
   var stickerNO = url.searchParams.get("sticker_no");
 
   document.getElementById("brand").innerText = url.searchParams.get("brand");
-  document.getElementById("product_name").innerText = url.searchParams.get(
-    "product_name"
-  );
-  document.getElementById("air_type").innerText = url.searchParams.get(
-    "air_type"
-  );
-  document.getElementById("power_phase").innerText = url.searchParams.get(
-    "power_phase"
-  );
-  document.getElementById("power_volt").innerText = url.searchParams.get(
-    "power_volt"
-  );
-  document.getElementById("capacity_cool").innerText = url.searchParams.get(
-    "capacity_cool"
-  );
+  document.getElementById("product_name").innerText = url.searchParams.get("product_name");
+  document.getElementById("air_type").innerText = url.searchParams.get("air_type");
+  document.getElementById("power_phase").innerText = url.searchParams.get("power_phase");
+  document.getElementById("power_volt").innerText = url.searchParams.get("power_volt");
+  document.getElementById("capacity_cool").innerText = url.searchParams.get("capacity_cool");
   document.getElementById("air_volume").innerHTML = airVolume;
   document.getElementById("air_kg").innerText = url.searchParams.get("air_kg");
   document.getElementById("year").innerText = url.searchParams.get("year");
   document.getElementById("company_name").innerHTML = comPany;
-  document.getElementById("company_address").innerText = url.searchParams.get(
-    "company_address"
-  );
-  document.getElementById("case_no").innerText = url.searchParams.get(
-    "case_no"
-  );
+  document.getElementById("company_address").innerText = url.searchParams.get("company_address");
+  document.getElementById("case_no").innerText = url.searchParams.get("case_no");
   document.getElementById("sticker_no").innerText = stickerNO;
 
   JsBarcode("#barcode_no", barcodeNo, {
@@ -59,28 +45,28 @@ function getTableData() {
 }
 getTableData();
 
-html2canvas(document.body, {
-  onrendered: function (canvas) {
-    var pageData = canvas.toDataURL("image/img", 1.0);
-    var pdf = new jsPDF("", "pt", "a4");
-    canvas.mozImageSmoothingEnabled = false;
-    canvas.webkitImageSmoothingEnabled = false;
-    canvas.msImageSmoothingEnabled = false;
-    canvas.imageSmoothingEnabled = false;
-    //addImage后两个参数控制添加图片的尺寸，此处将页面高度按照a4纸宽高比列进行压缩
-    pdf.addImage(pageData, "svg", 0, 0, canvas.width * 2, canvas.height * 2);
+// html2canvas(document.body, {
+//   onrendered: function (canvas) {
+//     var pageData = canvas.toDataURL("image/img", 1.0);
+//     var pdf = new jsPDF("", "pt", "a4");
+//     canvas.mozImageSmoothingEnabled = false;
+//     canvas.webkitImageSmoothingEnabled = false;
+//     canvas.msImageSmoothingEnabled = false;
+//     canvas.imageSmoothingEnabled = false;
+//     //addImage后两个参数控制添加图片的尺寸，此处将页面高度按照a4纸宽高比列进行压缩
+//     pdf.addImage(pageData, "svg", 0, 0, canvas.width * 2, canvas.height * 2);
 
-    pdf.save("test.pdf");
-  },
-});
-var getPixelRatio = function (context) {
-  var backingStore =
-    context.backingStorePixelRatio ||
-    context.webkitBackingStorePixelRatio ||
-    context.mozBackingStorePixelRatio ||
-    context.msBackingStorePixelRatio ||
-    context.oBackingStorePixelRatio ||
-    context.backingStorePixelRatio ||
-    1;
-  return (window.devicePixelRatio || 1) / backingStore;
-};
+//     pdf.save("test.pdf");
+//   },
+// });
+// var getPixelRatio = function (context) {
+//   var backingStore =
+//     context.backingStorePixelRatio ||
+//     context.webkitBackingStorePixelRatio ||
+//     context.mozBackingStorePixelRatio ||
+//     context.msBackingStorePixelRatio ||
+//     context.oBackingStorePixelRatio ||
+//     context.backingStorePixelRatio ||
+//     1;
+//   return (window.devicePixelRatio || 1) / backingStore;
+// };
--- js/canvas.js
+++ js/canvas.js
@@ -31,31 +31,30 @@ $(document).ready(() => {
   $("#svg").append($("#barcode").children()[1]);
 });
 
-var svg = document.getElementById("svg");
-//get svg source.
-var serializer = new XMLSerializer();
-var source = serializer.serializeToString(svg);
+// var svg = document.getElementById("svg");
+// //get svg source.
+// var serializer = new XMLSerializer();
+// var source = serializer.serializeToString(svg);
 
-//add name spaces. SVG崁入html
-if (!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)) {
-  source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
-}
-if (!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)) {
-  source = source.replace(
-    /^<svg/,
-    '<svg xmlns:xlink="http://www.w3.org/1999/xlink"'
-  );
-}
+// //add name spaces. SVG崁入html
+// if (!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)) {
+//   source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+// }
+// if (!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)) {
+//   source = source.replace(
+//     /^<svg/,
+//     '<svg xmlns:xlink="http://www.w3.org/1999/xlink"'
+//   );
+// };
 
-//add xml declaration
-source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+// //add xml declaration
+// source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+// //convert svg source to URI data scheme.
+// var url = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source);
+// //set url value to a element's href attribute.
+// document.getElementById("link").href = url;
+// //you can download svg file by right click menu.
 
-//convert svg source to URI data scheme.
-var url = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source);
-
-//set url value to a element's href attribute.
-document.getElementById("link").href = url;
-//you can download svg file by right click menu.
 
 function reload() {
   
@@ -77,11 +76,13 @@ function reload() {
 
   //add xml declaration
   source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
-
   //convert svg source to URI data scheme.
   var url = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source);
-
   //set url value to a element's href attribute.
   document.getElementById("link").href = url;
   //you can download svg file by right click menu.
-}
+};
+
+var canVas = document.getElementById('svg');
+var canvasPdf = new jsPDF();
+canvasPdf.save('canvas.pdf');
